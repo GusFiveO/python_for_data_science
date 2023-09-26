28cf625381cda2ff412e6a95567198e7b754fe3b
Augustin LORAIN <alorain@corp.free.fr>
2023-09-26 17:38:20 +0200

    ex08 progress done

--- module00/ex08/Loading.py
+++ module00/ex08/Loading.py
@@ -1,34 +1,28 @@
 from time import sleep
 from tqdm import tqdm
+import sys
 import os
 
-def ft_tqdm(lst: range) -> None:
-	"""loading"""
-	length = len(lst)
-	term_width = os.get_terminal_size().columns
-	for i in lst:
-		if i == 0:
-			progress = 0
-		else:
-			progress =  round(i / length * 100) + 1
-		# 6 + len(range) * 2
-		print(f"{progress:3}%|", end="")
-		# progress_equal = '=' * round(i / length * term_width - 15)
-		# progress_space = ' ' * (term_width -11 - round(i / length * term_width - 15))
-		# print(progress_equal, progress_space, end="\r")
-		# progress_space too long so retour a la ligne
-		print(progress_equal, end="\r")
-		yield i
-	print()
+def ft_tqdm(lst):
+    total = len(lst)
+    term_size = os.get_terminal_size()
+    bar_length = term_size.columns - 8
+    for i, item in enumerate(lst):
+        yield item
+        progress = (i + 1) / total
+        filled_length = int(bar_length * progress)
+        bar = '=' * filled_length + '>' + '-' * (bar_length - filled_length)
+        percentage = f'{int(progress * 100):3}%'
+        sys.stdout.write(f'\r{percentage} [{bar}]')
+        sys.stdout.flush()
+    
+    sys.stdout.write('\n')
+    sys.stdout.flush()
 
 def main():
 	"""main"""
 	for i in ft_tqdm(range(133)):
-		sleep(0.05)
+		sleep(0.01)
 
 if __name__ == "__main__":
 	main()
-	# for i in tqdm(range(100)):
-	# 	sleep(0.005)
-	# for i in tqdm(range(333)):
-	# 	sleep(0.005)
