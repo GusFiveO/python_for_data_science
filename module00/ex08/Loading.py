4d4b8de4a3cb8a7fd6e9ce82431e5df0bd727524
Augustin LORAIN <alorain@corp.free.fr>
2023-09-27 14:03:16 +0200

    add the avancement in the loading bar

--- module00/ex08/Loading.py
+++ module00/ex08/Loading.py
@@ -6,22 +6,22 @@ import os
 def ft_tqdm(lst):
     total = len(lst)
     term_size = os.get_terminal_size()
-    bar_length = term_size.columns - 8
+    bar_length = term_size.columns - 8 - (len(str(total)) * 2 + 2)
     for i, item in enumerate(lst):
         yield item
         progress = (i + 1) / total
         filled_length = int(bar_length * progress)
         bar = '=' * filled_length + '>' + '-' * (bar_length - filled_length)
         percentage = f'{int(progress * 100):3}%'
-        sys.stdout.write(f'\r{percentage} [{bar}]')
+        sys.stdout.write(f'\r{percentage} [{bar}] {i:{len(str(total))}}/{total}')
         sys.stdout.flush()
     
-    sys.stdout.write('\n')
+    sys.stdout.write(f'\r{percentage} [{bar}] {i + 1:{len(str(total))}}/{total}\n')
     sys.stdout.flush()
 
 def main():
 	"""main"""
-	for i in ft_tqdm(range(133)):
+	for i in ft_tqdm(range(1383)):
 		sleep(0.01)
 
 if __name__ == "__main__":
