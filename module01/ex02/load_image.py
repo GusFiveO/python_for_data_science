775d304f584235ce5454556ca0e2ddf36b2ca3d8
Augustin LORAIN <alorain@corp.free.fr>
2023-09-27 17:02:30 +0200

    module01 ex02

---
+++ module01/ex02/load_image.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+
+def ft_load(path: str) -> np.array:
+	"""load an image, print its format and it pixels content in RGB format"""
+	img = cv.imread(path, mode='RGB')
+	if img is None:
+		print("Filepath gived as parameter is not valid")
+		return None
+	print(f"The shape of this image is: {img.shape}")
+	return img
+
