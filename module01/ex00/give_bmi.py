11a1e19722fd01a9aeffcbba5acee0177a89d4f4
Augustin LORAIN <alorain@corp.free.fr>
2023-09-27 16:17:01 +0200

    module01 ex00

---
+++ module01/ex00/give_bmi.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+	"""give_bmi"""
+	if len(height) != len(weight):
+		print("Both parameter should have the same length")
+		return None
+	bmi_list = list()
+	merged_list = np.column_stack((height, weight))
+	for height, weight in merged_list:
+		if not isinstance(height, float | int) or not isinstance(weight, float | int):
+			print("Both parameter should only contain float or integer")
+			return None
+		bmi_list.append(weight / np.square(height))
+	return bmi_list
+	
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+	"""apply_limit"""
+	for elem in bmi:
+		if not isinstance(elem, float | int):
+			print("Parameter should only contain float or integer")
+			return None
+	return np.array([True if elem >= limit else False for elem in bmi])
---
+++ module01/ex00/tester.py
@@ -0,0 +1,11 @@
+from give_bmi import give_bmi, apply_limit
+
+height = [2.71, 1.15]
+weight = [165.3, 38.4]
+invalid_weight = [38.4, "lol"]
+
+bmi = give_bmi(height, weight)
+print(bmi, type(bmi))
+print(apply_limit(bmi, 26))
+print(give_bmi(height, invalid_weight))
+print(apply_limit(invalid_weight, 26))
