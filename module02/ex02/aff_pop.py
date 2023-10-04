19bdd2e35ecc7ab198121e850efbbdcc9015e39c
Augustin LORAIN <alorain@corp.free.fr>
2023-10-04 14:33:13 +0200

    module02 ex02

---
+++ module02/ex02/aff_pop.py
@@ -0,0 +1,35 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def aff_pop():
+	"""aff_pop"""
+	population_total = load("population_total.csv")
+
+	population_total = population_total.select(["country"] + [str(year) for year in range(1800, 2050)])
+
+	population_total_by_country = population_total.rows_by_key(key=["country"], named=True)
+
+	years_, france_population = zip(*population_total_by_country["France"][0].items())
+	years_, belgium_population = zip(*population_total_by_country["Belgium"][0].items())
+
+	years = [str(year) for year in range(1800, 2050, 40)]
+
+	france_population = [float(pop[:-1]) for pop in france_population]
+	belgium_population = [float(pop[:-1]) for pop in belgium_population]
+
+	fig, ax = plt.subplots()
+	ax.plot(years_, france_population)
+	ax.plot(years_, belgium_population)
+	ax.set_title("Population Projections")
+	ax.set_ylabel("Population")
+	ax.set_xlabel("Year")
+	ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.0fM'))
+	ax.xaxis.set_ticks(years)
+	ax.yaxis.set_ticks([pop for pop in range(20, 80, 20)])
+	ax.legend(["Belgium", "France"], loc="lower right")
+	plt.show()
+
+
+if __name__ == "__main__":
+	aff_pop()
