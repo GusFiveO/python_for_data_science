f6c92bd09f50ce0ac3773e9f84426333400a1604
Augustin LORAIN <alorain@corp.free.fr>
2023-10-06 13:25:48 +0200

    module03 ex03

---
+++ module03/ex03/ft_calculator.py
@@ -0,0 +1,35 @@
+class calculator:
+	"""calculator class"""
+
+	def __init__(self, array: list):
+		"""init method"""
+		self.array = array
+
+	def __add__(self, object) -> None:
+		"""add method"""
+		for i in range(len(self.array)):
+			self.array[i] += object
+		print(self.array)
+
+	def __mul__(self, object) -> None:
+		"""mul method"""
+		for i in range(len(self.array)):
+			self.array[i] *= object
+		print(self.array)
+
+	def __sub__(self, object) -> None:
+		"""sub method"""
+		for i in range(len(self.array)):
+			self.array[i] -= object
+		print(self.array)
+
+	def __truediv__(self, object) -> None:
+		"""truediv method"""
+		try:
+			for i in range(len(self.array)):
+				self.array[i] /= object
+			print(self.array)
+		except ZeroDivisionError as e:
+			print("Error: cannot divide by zero")
+
+	
