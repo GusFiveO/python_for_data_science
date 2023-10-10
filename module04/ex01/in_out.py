d5352826195104dfb64eeb159f3dfde317ca47f6
Augustin LORAIN <alorain@corp.free.fr>
2023-10-10 15:48:43 +0200

    module04 ex01

---
+++ module04/ex01/in_out.py
@@ -0,0 +1,17 @@
+def square(x: int | float) -> int | float:
+	"""square function"""
+	return x**2
+
+def pow(x: int | float) -> int | float:
+	"""pow function"""
+	return x**x
+
+def outer(x: int | float, function) -> object:
+	"""outer function"""
+	count = 0
+	def inner() -> float:
+		"""inner function"""
+		nonlocal x
+		x = function(x)
+		return x
+	return inner
