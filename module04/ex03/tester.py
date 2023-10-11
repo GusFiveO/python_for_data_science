273d2706d9ab6a88b30fb1da802175a99661fd5c
Augustin LORAIN <alorain@corp.free.fr>
2023-10-11 15:05:00 +0200

    module04 ex03 add test

--- module04/ex03/tester.py
+++ module04/ex03/tester.py
@@ -1,3 +1,5 @@
 from new_student import Student
 student = Student(name = "Edward", surname = "agle")
 print(student)
+student2 = Student(name = "Edward", surname = "agle", id = "toto")
+print(student)
