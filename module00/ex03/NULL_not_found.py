7c91be17faee716b3a074d0c82f6c70510063375
Augustin LORAIN <alorain@bess-f2r4s16.clusters.42paris.fr>
2024-01-10 17:00:03 +0100

    fixed(module00ex03): did the exercise in a more python way

--- module00/ex03/NULL_not_found.py
+++ module00/ex03/NULL_not_found.py
@@ -1,14 +1,21 @@
+def isNan(value):
+	return value != value
+
 def NULL_not_found(obj: any) -> int:
-	if obj is None:
-		print(f"{obj}: <class 'NoneType'>")
-	elif isinstance(obj, float) and str(obj) == 'nan':
-		print(f"Cheese: {obj} <class 'float'>")
-	elif obj == 0:
-		print(f"Zero: {obj} <class 'int'>")
-	elif obj == '':
-		print(f"Empty: <class 'str'>")
-	elif isinstance(obj, bool):
-		print(f"Fake: {obj} <class 'bool'>")
+	trick_dict = {
+		None: "Nothing",
+		float("NaN"): "Cheese",
+		0: "Zero",
+		'': "Empty",
+		False: "Fake"
+	}
+
+	associated_string = trick_dict.get(obj)
+
+	if associated_string:
+		print(f'{associated_string}: {obj} {type(obj)}')
+	elif isNan(obj):
+		print(f'Cheese: {obj} {type(obj)}')
 	else:
 		print("Type not Found")
 		return 1
