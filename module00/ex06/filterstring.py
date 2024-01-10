82dd738eec4cdc7544e53af143c448b6a2d8f731
Augustin LORAIN <alorain@bess-f2r4s16.clusters.42paris.fr>
2024-01-10 19:01:56 +0100

    fixed(module00ex06): use of list comprehension and accept function as None

--- module00/ex06/filterstring.py
+++ module00/ex06/filterstring.py
@@ -1,19 +1,22 @@
 import sys
 from ft_filter import ft_filter
 
+
 def check_if_string_long_enough(string, number):
-	"""check if a string is long enough"""
-	if len(string) < number:
-		return False
-	return True
+    """check if a string is long enough"""
+    if len(string) < number:
+        return False
+    return True
+
 
 if __name__ == "__main__":
-	if len(sys.argv) > 3:
-		print("AssertionError: the arguments are bad")
-	else:
-		try:
-			word_list = sys.argv[1].split()
-			filtered_list = ft_filter(lambda string: len(string) > int(sys.argv[2]), word_list)
-			print(filtered_list)
-		except:
-			print("AssertionError: the arguments are bad")
+    if len(sys.argv) > 3:
+        print("AssertionError: the arguments are bad")
+    else:
+        try:
+            word_list = sys.argv[1].split()
+            filtered_list = ft_filter(lambda string: len(string) >
+                                      int(sys.argv[2]), word_list)
+            print(filtered_list)
+        except Exception:
+            print("AssertionError: the arguments are bad")
--- module00/ex06/ft_filter.py
+++ module00/ex06/ft_filter.py
@@ -1,29 +1,34 @@
-import sys
-
 def ft_filter(check_function, _list):
-	"""filter(function or None, iterable) --> filter object
+    """filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true."""
-	filtered_list = []
-	for elem in _list:
-		if check_function(elem) == True:
-			filtered_list.append(elem)
-	return filtered_list
+    filtered_list = [elem for elem in _list if check_function
+                     is None or check_function(elem) is True]
+    return filtered_list
 
 
 def check_even(number):
+    """returning a boolean depending if the number passed in
+     parameter is Even or not"""
     if number % 2 == 0:
-          return True  
+        return True
     return False
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
 def main():
-	print(ft_filter(check_even, numbers))
-#	print(filter.__doc__)
-#	print(ft_filter.__doc__)
+    print(ft_filter(check_even, numbers))
+    print(list(filter(check_even, numbers)))
+    print(ft_filter(None, numbers))
+    print(list(filter(None, numbers)))
+    print(ft_filter(None, 4))
+    print(list(filter(None, 4)))
+    print(filter.__doc__)
+    print(ft_filter.__doc__)
+
 
 if __name__ == "__main__":
-	main()
+    main()
