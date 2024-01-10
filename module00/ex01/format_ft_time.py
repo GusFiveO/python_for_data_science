9e15b3e4a97528fa80a052140835d52ab2f70873
Augustin LORAIN <alorain@bess-f2r4s16.clusters.42paris.fr>
2024-01-10 16:25:06 +0100

    fixed(module00ex01): add ',' separator and precision for the timestamp

--- module00/ex01/format_ft_time.py
+++ module00/ex01/format_ft_time.py
@@ -3,5 +3,5 @@ from datetime import datetime
 
 timestamp = time.time()
 
-print(f"Seconds since Junary 1, 1970: {timestamp}, or {timestamp:.3g} in scientific notation")
+print(f"Seconds since Junary 1, 1970: {timestamp:,.3f} or {timestamp:.3g} in scientific notation")
 print(datetime.now().strftime("%b %d %Y"))
