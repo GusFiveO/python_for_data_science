224f30b6a09c73feb5036084d5460fc24acb149e
Augustin LORAIN <alorain@paul-f4Br7s2.clusters.42paris.fr>
2024-01-12 19:24:35 +0100

    fixed(module02ex01): norm and use of a different backend for matplotlib to work well on session

--- module02/ex01/aff_life.py
+++ module02/ex01/aff_life.py
@@ -1,20 +1,27 @@
 from load_csv import load
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')  # Use Tkinter backend
+
 
 def aff_life():
-	"""aff_life"""
-	life_expectancy_years = load("life_expectancy_years.csv")
+    """aff_life"""
+    life_expectancy_years = load("life_expectancy_years.csv")
+    if life_expectancy_years is None:
+        return None
+    country_life_expectancy_list = life_expectancy_years.rows_by_key(
+        key=["country"], named=True
+    )
+    x, y = zip(*country_life_expectancy_list["France"][0].items())
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_ylabel("Life expectancy")
+    ax.set_xlabel("Year")
+    ax.set_title("France Life expectancy Projections")
+    labels = [str(year) for year in range(1800, 2100, 40)]
+    ax.set_xticks(labels)
+    plt.show(block=True)
 
-	country_life_expectancy_list = life_expectancy_years.rows_by_key(key=["country"], named=True)
-	x, y = zip(*country_life_expectancy_list["France"][0].items())
-	fig, ax = plt.subplots()
-	ax.plot(x, y)
-	ax.set_ylabel('Life expectancy')
-	ax.set_xlabel('Year')
-	ax.set_title('France Life expectancy Projections')
-	labels = [str(year) for year in range(1800, 2100, 40)]
-	ax.set_xticks(labels)
-	plt.show()
 
 if __name__ == "__main__":
-	aff_life()
+    aff_life()
