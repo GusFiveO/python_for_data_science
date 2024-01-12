4f94542e68e6dea696997777cf36ad87ed3da499
Augustin LORAIN <alorain@paul-f4Br7s2.clusters.42paris.fr>
2024-01-12 18:59:36 +0100

    fixed(module02ex00): norm and now catch all exception

--- module02/ex00/load_csv.py
+++ module02/ex00/load_csv.py
@@ -1,11 +1,12 @@
 import polars
 
+
 def load(path: str):
-	"""load a csv file"""
-	csv = {}
-	try:
-		csv = polars.read_csv(path)
-	except FileNotFoundError as error:
-		print(error)
-		return None
-	return csv
+    """load a csv file"""
+    csv = {}
+    try:
+        csv = polars.read_csv(path)
+    except Exception as e:
+        print(e)
+        return None
+    return csv
