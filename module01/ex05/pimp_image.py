3e2a78a6b91406bd06315f12de938a12fe462ede
Augustin LORAIN <alorain@paul-f4Br7s3.clusters.42paris.fr>
2024-01-12 17:27:54 +0100

    fixed(module01ex05): norm, fixed grey scale and error handling

--- module01/ex05/pimp_image.py
+++ module01/ex05/pimp_image.py
@@ -1,42 +1,64 @@
-from load_image import ft_load
-import numpy as np
 import cv2 as cv
 
+
 def ft_invert(arr):
-	"""invert color of an image"""
-	return 255 - arr
+    """invert color of an image"""
+    try:
+        return 255 - arr
+    except Exception:
+        print(Exception)
+        return None
+
 
 def ft_red(arr):
-	"""only keep red pixels of an image"""
-	red_arr = arr.copy()
-	red_arr[:, :, 0] = 0
-	red_arr[:, :, 1] = 0
-	return red_arr
+    """only keep red pixels of an image"""
+    try:
+        red_arr = arr.copy()
+        red_arr[:, :, 0] = 0
+        red_arr[:, :, 1] = 0
+        return red_arr
+    except Exception:
+        print(Exception)
+        return None
+
 
 def ft_green(arr):
-	"""only keep green pixels of an image"""
-	green_arr = arr.copy()
-	green_arr[:, :, 0] = 0
-	green_arr[:, :, 2] = 0
-	return green_arr
+    """only keep green pixels of an image"""
+    try:
+        green_arr = arr.copy()
+        green_arr[:, :, 0] = 0
+        green_arr[:, :, 2] = 0
+        return green_arr
+    except Exception:
+        print(Exception)
+        return None
+
 
 def ft_blue(arr):
-	"""only keep blue pixels of an image"""
-	blue_arr = arr.copy()
-	blue_arr[:, :, 1] = 0
-	blue_arr[:, :, 2] = 0
-	return blue_arr
+    """only keep blue pixels of an image"""
+    try:
+        blue_arr = arr.copy()
+        blue_arr[:, :, 1] = 0
+        blue_arr[:, :, 2] = 0
+        return blue_arr
+    except Exception:
+        print(Exception)
+        return None
+
 
 def ft_grey(arr):
-	"""make an image become balck and white"""
-	r_coeff = 0.2989
-	g_coeff = 0.5870
-	b_coeff = 0.1140
-	array_copy = np.zeros_like(arr)
-	for i in range(array_copy.shape[0]):
-		for j in range(array_copy.shape[1]):
-			pixel = arr[i, j]
-			value = pixel[0] * r_coeff + pixel[1] * g_coeff + pixel[2] * b_coeff
-			array_copy[i, j] = np.repeat(value, 3)
-	return array_copy
+    """convert an image to grey scale"""
+    try:
+        blue_channel, green_channel, red_channel = cv.split(arr)
 
+        gray_image = cv.addWeighted(blue_channel, 1/3, green_channel, 1/3, 0)
+        gray_image = cv.addWeighted(gray_image, 1, red_channel, 1/3, 0)
+
+        gray_image = gray_image.astype('uint8')
+
+        gray_image = cv.merge([gray_image, gray_image, gray_image])
+
+        return gray_image
+    except Exception:
+        print(Exception)
+        return None
