0ff9a9cd4fc64072f5aa1a386c11944c95d4405c
Augustin LORAIN <alorain@paul-f4Br7s3.clusters.42paris.fr>
2024-01-12 14:58:23 +0100

    modified(module00ex09): not able to install the  package proprely

---
+++ module00/ex09/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    author='alorain',
+    author_email='alorain@42.fr',
+    description='A sample test package',
+    long_description='A sample test package for demonstration purposes.',
+    # url='https://github.com/eagle/ft_package',
+    license_files=('LICENSE.txt',),
+    packages=find_packages(),
+    classifiers=[],
+    install_requires=[],
+)
