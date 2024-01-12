f46f586a7b0546e7d43321a18015ff21e997371a
Augustin LORAIN <alorain@paul-f4Br7s3.clusters.42paris.fr>
2024-01-12 16:12:35 +0100

    fixed(module01ex01): norm and Error handling

--- module01/ex01/array2D.py
+++ module01/ex01/array2D.py
@@ -1,12 +1,19 @@
 import numpy as np
 
+
 def slice_me(family: list, start: int, end: int) -> list:
-	"""slice_me"""
-	if not isinstance(family, list):
-		print("Parameter must be a list")
-		return None
-	np_family = np.array(family)
-	new_family = np_family[start:end]
-	print(f"My shape is : {np_family.shape}")
-	print(f"My new shape is : {new_family.shape}")
-	return new_family
+    """slice_me"""
+    if not isinstance(family, list):
+        print("First parameter must be a list")
+        return None
+    if len(family) and not all(len(elem) == len(family[0]) for elem in family):
+        print("First parameter must be a list of same size list")
+        return None
+    try:
+        np_family = np.array(family)
+        new_family = np_family[start:end]
+        print(f"My shape is : {np_family.shape}")
+        print(f"My new shape is : {new_family.shape}")
+        return new_family
+    except Exception:
+        print("Last two parameter must be integer")
