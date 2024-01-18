ce9547c53cca37159ff22dcbf27fe2aec7b443d4
Augustin LORAIN <alorain@bess-f4r4s1.clusters.42paris.fr>
2024-01-18 17:51:46 +0100

    fixed(module04ex01): norm

--- module04/ex01/in_out.py
+++ module04/ex01/in_out.py
@@ -1,17 +1,19 @@
 def square(x: int | float) -> int | float:
-	"""square function"""
-	return x**2
+    """square function"""
+    return x**2
+
 
 def pow(x: int | float) -> int | float:
-	"""pow function"""
-	return x**x
+    """pow function"""
+    return x**x
+
 
 def outer(x: int | float, function) -> object:
-	"""outer function"""
-	count = 0
-	def inner() -> float:
-		"""inner function"""
-		nonlocal x
-		x = function(x)
-		return x
-	return inner
+    """outer function"""
+    def inner() -> float:
+        """inner function"""
+        nonlocal x
+        x = function(x)
+        return x
+
+    return inner
