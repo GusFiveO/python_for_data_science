e198f95a9cf7907d00923098de1995473abb17b2
Augustin LORAIN <alorain@bess-f4r4s1.clusters.42paris.fr>
2024-01-18 17:12:25 +0100

    fixed(module03ex01): norm

--- module03/ex01/S1E7.py
+++ module03/ex01/S1E7.py
@@ -1,49 +1,51 @@
 from S1E9 import Character
 
+
 class Baratheon(Character):
-	"""Baratheon class"""
-	
-	def __init__(self, first_name: str, is_alive: bool = True):
-		"""__init__ method"""
-		self.first_name = first_name
-		self.is_alive = is_alive
-		self.family_name = "Baratheon"
-		self.eyes = "brown"
-		self.hairs = "dark"
+    """Baratheon class"""
 
-	def __str__(self):
-		return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """__init__ method"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
 
-	def __repr__(self):
-		return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-	def die(self):
-		"""die method"""
-		if self.is_alive:
-			self.is_alive = False
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def die(self):
+        """die method"""
+        if self.is_alive:
+            self.is_alive = False
+
 
 class Lannister(Character):
-	"""Lannister Class"""
+    """Lannister Class"""
 
-	def __init__(self, first_name: str, is_alive: bool = True):
-		"""__init__ method"""
-		self.first_name = first_name
-		self.is_alive = is_alive
-		self.family_name = "Lannister"
-		self.eyes = "blue"
-		self.hairs = "light"
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """__init__ method"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
 
-	def __str__(self):
-		return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-	def __repr__(self):
-		return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-	def create_lannister(first_name: str, is_alive: bool):
-		"""create a lannister"""
-		return Lannister(first_name, is_alive)
+    def create_lannister(first_name: str, is_alive: bool):
+        """create a lannister"""
+        return Lannister(first_name, is_alive)
 
-	def die(self):
-		"""die method"""
-		if self.is_alive:
-			self.is_alive = False
+    def die(self):
+        """die method"""
+        if self.is_alive:
+            self.is_alive = False
