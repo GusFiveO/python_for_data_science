8ad64b15e388f9411dddf9047f87d0ebfab726cd
Augustin LORAIN <alorain@bess-f4r4s1.clusters.42paris.fr>
2024-01-18 17:18:49 +0100

    fixed(module03ex02): norm

--- module03/ex02/DiamondTrap.py
+++ module03/ex02/DiamondTrap.py
@@ -1,20 +1,21 @@
 from S1E7 import Baratheon, Lannister
 
+
 class King(Baratheon, Lannister):
-	"""King Class"""
+    """King Class"""
 
-	def set_eyes(self, eyes: str):
-		"""change the eyes attribute"""
-		self.eyes = eyes
+    def set_eyes(self, eyes: str):
+        """change the eyes attribute"""
+        self.eyes = eyes
 
-	def set_hairs(self, hairs: str):
-		"""change the hairs attribute"""
-		self.hairs = hairs
+    def set_hairs(self, hairs: str):
+        """change the hairs attribute"""
+        self.hairs = hairs
 
-	def get_eyes(self):
-		"""return the eyes attribute"""
-		return self.eyes
+    def get_eyes(self):
+        """return the eyes attribute"""
+        return self.eyes
 
-	def get_hairs(self):
-		"""return the hairs attribute"""
-		return self.hairs
+    def get_hairs(self):
+        """return the hairs attribute"""
+        return self.hairs
--- module03/ex02/S1E9.py
+++ module03/ex02/S1E9.py
@@ -1,26 +1,30 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
-	"""Class Character"""
-	def __init__(self, first_name: str, is_alive: bool = True):
-		"""Constructor"""
-		self.first_name = first_name
-		self.is_alive = is_alive
-	
-	@abstractmethod
-	def die(self):
-		pass
-		"""make the character die"""
-		if self.is_alive:
-			self.is_alive = False
+    """Class Character"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        pass
+        """make the character die"""
+        if self.is_alive:
+            self.is_alive = False
+
 
 class Stark(Character):
-	"""Stark Class"""
-	def __init(self):
-		self
-	def die(self):
-		pass
-		"""make Stark die"""
-		if self.is_alive:
-			self.is_alive = False
+    """Stark Class"""
 
+    def __init(self):
+        self
+
+    def die(self):
+        pass
+        """make Stark die"""
+        if self.is_alive:
+            self.is_alive = False
