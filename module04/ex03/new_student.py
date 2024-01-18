9fe86d79a6977cdb3ce562527a6a6354ee11d4e1
Augustin LORAIN <alorain@bess-f4r4s1.clusters.42paris.fr>
2024-01-18 18:06:22 +0100

    fixed(module04ex03): norm

--- module04/ex03/new_student.py
+++ module04/ex03/new_student.py
@@ -1,23 +1,24 @@
 import random
 import string
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+
 
 def generate_id() -> str:
-	return "".join(random.choices(string.ascii_lowercase, k = 15))
+    return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
-	name: str
-	surname: str
-	active: bool
-	login: str
-	id: str
+    name: str
+    surname: str
+    active: bool
+    login: str
+    id: str
 
-	def __init__(self, name: str, surname: str):
-		"""init function"""
-		self.name = name
-		self.surname = surname
-		self.active = True
-		self.login = f"{name[0]}{surname}"
-		self.id = generate_id()
+    def __init__(self, name: str, surname: str):
+        """init function"""
+        self.name = name
+        self.surname = surname
+        self.active = True
+        self.login = f"{name[0]}{surname}"
+        self.id = generate_id()
--- module04/ex03/tester.py
+++ module04/ex03/tester.py
@@ -1,5 +1,9 @@
 from new_student import Student
-student = Student(name = "Edward", surname = "agle")
+
+student = Student(name="Edward", surname="agle")
 print(student)
-student2 = Student(name = "Edward", surname = "agle", id = "toto")
-print(student)
+try:
+    student2 = Student(name="Edward", surname="agle", id="toto")
+    print(student)
+except Exception as e:
+    print(e)
