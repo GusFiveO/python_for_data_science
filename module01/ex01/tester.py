55df701d84a819760fe9ef7a4f670ed40601a3f1
Augustin LORAIN <alorain@paul-f4Ar4s3.clusters.42paris.fr>
2024-01-24 15:01:53 +0100

    module01ex01

--- module01/ex01/tester.py
+++ module01/ex01/tester.py
@@ -3,7 +3,13 @@ from array2D import slice_me
 family = [[1.80, 78.4], [2.15, 102.7], [2.10, 98.5], [1.88, 75.2]]
 
 print(slice_me(family, 0, 2))
+print()
 print(slice_me(family, 1, -2))
-print(slice_me("LOL", 1, -2))
-print(slice_me([], 1, -2))
-print(slice_me([[10], [20, 10]], '1', '-2'))
+print()
+print(slice_me("LOL", 1, -2))                   # first parameter is a string
+print()
+print(slice_me([], 1, -2))                      # empty list
+print()
+print(slice_me([[10], [20, 10]], '1', '-2'))    # not same size list
+print()
+print(slice_me(family, '1', '-2'))              # last parameter not intergers
