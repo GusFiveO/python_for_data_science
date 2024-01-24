648e4db7b98e06d43efb1193de392a4efa610518
Augustin LORAIN <alorain@paul-f4Ar4s3.clusters.42paris.fr>
2024-01-24 14:53:30 +0100

    module01ex00

--- module01/ex00/tester.py
+++ module01/ex00/tester.py
@@ -7,7 +7,8 @@ invalid_weight = [38.4, "lol"]
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
-print(give_bmi(height, invalid_weight))
-print(apply_limit(invalid_weight, 26))
-print(give_bmi(26, 26))
-print(apply_limit(26, 26))
+print(give_bmi(height, invalid_weight))         # contain a string
+print(give_bmi(height, invalid_weight[:-1]))    # not the same size
+print(apply_limit(invalid_weight, 26))          # contain a string
+print(give_bmi(26, 26))                         # not iterable
+print(apply_limit(26, 26))                      # not iterable
