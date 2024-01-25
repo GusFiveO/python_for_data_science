c2f8ae174961540810a2e22cc12af1435581dd40
Augustin LORAIN <alorain@paul-f4Ar7s4.clusters.42paris.fr>
2024-01-25 15:57:14 +0100

    cleaned(module03ex01): code cleaned

--- module03/ex01/S1E9.py
+++ module03/ex01/S1E9.py
@@ -5,26 +5,20 @@ class Character(ABC):
     """Class Character"""
 
     def __init__(self, first_name: str, is_alive: bool = True):
-        """Constructor"""
+        """Character constructor"""
         self.first_name = first_name
         self.is_alive = is_alive
 
     @abstractmethod
     def die(self):
-        """make the character die"""
+        """Character die method"""
         pass
-        if self.is_alive:
-            self.is_alive = False
 
 
 class Stark(Character):
     """Stark Class"""
 
-    def __init(self):
-        self
-
     def die(self):
-        """make Stark die"""
-        pass
+        """Stark die method"""
         if self.is_alive:
             self.is_alive = False
