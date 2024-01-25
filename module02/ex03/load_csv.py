55e77fa4d3e979ffebfba95cd8fe9284edeee2f7
Augustin LORAIN <alorain@bess-f4r1s4.clusters.42paris.fr>
2024-01-25 12:35:33 +0100

    fixed(module02ex03): print the dimensions

--- module02/ex03/load_csv.py
+++ module02/ex03/load_csv.py
@@ -1,11 +1,12 @@
 import pandas
 
 
-def load(path: str):
+def load(path: str) -> pandas.DataFrame:
     """load a csv file"""
     csv = {}
     try:
         csv = pandas.read_csv(path)
+        print(f"Loading dataset of dimensions {csv.shape}")
     except Exception as e:
         print(e)
         return None
