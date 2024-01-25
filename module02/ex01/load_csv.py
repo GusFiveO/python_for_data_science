901ad96f19bb87f7a4e17ba1b47d9e12400a4093
Augustin LORAIN <alorain@bess-f4r1s4.clusters.42paris.fr>
2024-01-25 12:31:20 +0100

    fixed(module02ex01): print the dimensions

--- module02/ex01/load_csv.py
+++ module02/ex01/load_csv.py
@@ -1,11 +1,12 @@
 import polars
 
 
-def load(path: str):
+def load(path: str) -> polars.DataFrame:
     """load a csv file"""
     csv = {}
     try:
         csv = polars.read_csv(path)
+        print(f"Loading dataset of dimensions {csv.shape}")
     except Exception as e:
         print(e)
         return None
